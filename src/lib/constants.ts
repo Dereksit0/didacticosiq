3e75e7b93f127a477083c9aea5a0499c0ae6d0f1
Dereksit0 <128322660+Dereksit0@users.noreply.github.com>
2026-05-10 15:02:28 -0600

    Cambios finales

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,6 +5,6 @@ export const WA_MAYOREO_LINK =
   "https://wa.me/522222314611?text=%C2%A1Hola%20DidacticosIQ!%20Me%20interesa%20solicitar%20una%20cotizaci%C3%B3n%20de%20mayoreo%20y%20conocer%20m%C3%A1s%20de%20sus%20productos.";
 
 export const CATALOG_LINK =
-  "https://drive.google.com/file/d/1QZwfOiraa9Cx6kozdybGau8DzEA6x28x/view";
+  "https://drive.google.com/file/d/1PxW4Jo3H-w0EuhjywuBlDC6at76Is4Ze/view?usp=sharing";
 
 export const MAPS_LINK = "https://maps.app.goo.gl/thC14C7URbyhBiyk9";
